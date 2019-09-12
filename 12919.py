0d02d305adbb9f25cf7dedd480b975b557b88f59
Ingólfur Orri <ingolfurorri@gmail.com>
2019-09-12 13:25:12 +0000

    qwe

---
+++ 12919.py
@@ -0,0 +1,20 @@
+d = int(input("Type in the exercise you want to run: "))
+
+if(d == 1):
+    e = 2
+    e = 4
+
+elif(d == 2):
+    e = 2
+
+elif(d == 3):
+    e = 2
+
+elif(d == 4):
+    e = 2
+
+elif(d == 5):
+    e = 2
+
+elif(d == 6):
+    e = 2
